1e00bf17bc94c81108306e6f2b5b783b150f6e74
uni897 <akhasmeer7@gmail.com>
2025-12-17 14:00:37 -0500

    jdytfhgfhfg

--- app/src/main.js
+++ app/src/main.js
@@ -3,6 +3,7 @@ import './style.css'
 const apiKey = "hXa8noH6212PYfVLSPB32jVH3zAvGcGB"
 async function getData(params) {
     try{
+        
         //get data from api
         const response = await fetch(`https://api.currencyscoop.com/v1/latest?api_key=${apiKey}`)
         if(response.status != 200){
@@ -10,11 +11,12 @@ async function getData(params) {
         }else{
             //converts response into json we can use
             const data = await response.json()
+            console.log("success")
             console.log(data)
         }
     }catch(error){
-        console.log(error)
+        console.log(error + " Grrrr")
     }
 }
-
+getData()
 //https://api.currencyscoop.com/v1/convert?api_key=hXa8noH6212PYfVLSPB32jVH3zAvGcGB&from=USD&to=EUR&amount=600000
