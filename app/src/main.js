53860489e5d382e818a6e2d1513312bda1af6a79
uni897 <akhasmeer7@gmail.com>
2025-12-18 14:02:11 -0500

    hgk gchkhgjkhgkchujv

--- app/src/main.js
+++ app/src/main.js
@@ -1,11 +1,12 @@
 import './style.css'
 
+///api/v1/convert?api_key=hXa8noH6212PYfVLSPB32jVH3zAvGcGB&from=USD&to=EUR&amount=600000
 const apiKey = "hXa8noH6212PYfVLSPB32jVH3zAvGcGB"
 async function getData(params) {
     try{
         
         //get data from api
-        const response = await fetch(`https://api.currencyscoop.com/v1/latest?api_key=${apiKey}`)
+        const response = await fetch(`/api/v1/latest?api_key=${apiKey}`)
         if(response.status != 200){
             throw new Error(response)
         }else{
@@ -15,8 +16,7 @@ async function getData(params) {
             console.log(data)
         }
     }catch(error){
-        console.log(error + " Grrrr")
+        console.log(error)
     }
 }
 getData()
-//https://api.currencyscoop.com/v1/convert?api_key=hXa8noH6212PYfVLSPB32jVH3zAvGcGB&from=USD&to=EUR&amount=600000
--- app/vite.config.js
+++ app/vite.config.js
@@ -5,4 +5,16 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+
+   server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.currencyscoop.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
+
+
